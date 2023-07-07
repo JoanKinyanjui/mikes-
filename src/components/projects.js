e7e252dc7019cf51ffeb44977bace50e02455889
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2023-07-07 10:24:16 +0300

    add links of projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,32 +2,38 @@ const projects = [
     {    id:1,
         image: "/images/img1.png",
         name: "Swypt",
-        tagLine: "Defi app bridging crypto and African fiat currency"
+        tagLine: "Defi app bridging crypto and African fiat currency",
+        link:"https://swypt.io/"
     },
     {    id:2,
         image: "/images/img2.png",
         name: "Savannah Arts ",
-        tagLine: "Virtual art gallery. Metaverse and NFT."
+        tagLine: "Virtual art gallery. Metaverse and NFT.",
+        link:"https://www.spatial.io/s/SUNSET-SAVANNAH-COLLECTION-6474bc23beec827ded58d953?share=8936219542412280757"
     },
     {    id:1,
         image: "/images/project3.png",
         name: "Elamak Networks",
-        tagLine:"Website for an ISP."
+        tagLine:"Website for an ISP.",
+        link:""
     },
     {    id:4,
         image: "/images/project4.png",
         name: "BBA",
-        tagLine: "Building Africa’s largest blockchain resource center."
+        tagLine: "Building Africa’s largest blockchain resource center.",
+        link:"https://www.figma.com/proto/jyfhV1ErVvap2vwmXbPqjz/BBA?node-id=1-2&starting-point-node-id=119%3A1386"
     },
     {    id:5,
         image: "/images/img2.png",
         name: "Savannah Arts",
-        tagLine: "Virtual art gallery. Metaverse and NFT."
+        tagLine: "Virtual art gallery. Metaverse and NFT.",
+        link:"https://www.spatial.io/s/SUNSET-SAVANNAH-COLLECTION-6474bc23beec827ded58d953?share=8936219542412280757"
     },
     {    id:6,
         image: "/images/project6.png",
         name: "AI Product Creators",
-        tagLine: "Website for an AI themed podcast and talk show."
+        tagLine: "Website for an AI themed podcast and talk show.",
+        link:""
     }
 ];
 
